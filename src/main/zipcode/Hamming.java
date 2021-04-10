2009b96ba400f766d657ab9f5caadf78efd90429
Monali <khandelwal.monali@gmail.com>
2021-04-10 16:10:36 -0400

    lab completed

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,25 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
-    }
+    String hammingString1;
+    String hammingString2;
+
+    public Hamming(String s, String s1) throws IllegalArgumentException {
+        this.hammingString1 =s;
+        this.hammingString2 =s1;
+
+        if (hammingString1.length() != hammingString2.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        }
 
     public int getHammingDistance() {
-        return -1;
+    int count = 0;
+        for (int i = 0; i < hammingString1.length(); i++) {
+            if(hammingString1.charAt(i) != hammingString2.charAt(i)){
+                count++;
+            }
+        }
+    return count;
     }
 }
